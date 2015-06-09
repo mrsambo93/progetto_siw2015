8c9a25ed00d85ea1db300e135bed592c81d66f9f
mrsambo93 <sambora93@gmail.com>
2015-06-09 22:39:32 +0200

    primo commitù

---
+++ src/it/uniroma3/controller/ControllerProdotto.java
@@ -0,0 +1,106 @@
+package it.uniroma3.controller;
+
+import java.util.List;
+
+import it.uniroma3.model.FacadeProdotto;
+import it.uniroma3.model.Prodotto;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+
+@ManagedBean(name = "controllerProdotto")
+public class ControllerProdotto {
+	
+	@EJB
+	private FacadeProdotto facadeProdotto;
+	
+	private Long id;
+	private String nome;
+	private String codice;
+	private String descrizione;
+	private Float prezzo;	
+	private Integer qtaMagazzino;
+	private Prodotto prodotto;
+	private List<Prodotto> catalogoProdotti;
+	
+	public String creaProdotto(String nome, String codice, String descrizione, Float prezzo) {
+		this.prodotto = this.facadeProdotto.creaProdotto(nome, codice, descrizione, prezzo);
+		return "prodotto";	
+	}
+	
+	public String cercaCatalogoProdotti() {
+		this.catalogoProdotti = this.facadeProdotto.cercaCatalogoProdotti();
+		return "catalogoProdotti";
+	}
+	
+	public String cercaProdotto(Long id) {
+		this.prodotto = this.facadeProdotto.cercaProdotto(id);
+		return "prodotto";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Float getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(Float prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public Integer getQtaMagazzino() {
+		return qtaMagazzino;
+	}
+
+	public void setQtaMagazzino(Integer qtaMagazzino) {
+		this.qtaMagazzino = qtaMagazzino;
+	}
+
+	public Prodotto getProdotto() {
+		return prodotto;
+	}
+
+	public void setProdotto(Prodotto prodotto) {
+		this.prodotto = prodotto;
+	}
+
+	public List<Prodotto> getCatalogoProdotti() {
+		return catalogoProdotti;
+	}
+
+	public void setCatalogoProdotti(List<Prodotto> catalogoProdotti) {
+		this.catalogoProdotti = catalogoProdotti;
+	}
+	
+}
---
+++ src/it/uniroma3/model/FacadeProdotto.java
@@ -0,0 +1,42 @@
+package it.uniroma3.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@Stateless(name = "facadeProdotto")
+@NamedQuery(name = "trovaProdotti", query = "SELECT p FROM Prodotto p")
+public class FacadeProdotto {
+	
+	@PersistenceContext(unitName = "unit-progetto")
+	private EntityManager em;
+	
+	//UC4
+	public Prodotto creaProdotto(String nome, String codice, String descrizione, Float prezzo) {
+		Prodotto prodotto = new Prodotto(nome,codice,descrizione,prezzo);	
+		Prodotto temp = this.em.find(Prodotto.class, prodotto.getCodice());
+		if(temp != null) {
+			this.em.persist(prodotto);
+			return prodotto;
+		}
+		return null;
+	}
+	
+	//UC1,UC2
+	public List<Prodotto> cercaCatalogoProdotti() {
+		List<Prodotto> catalogo = new ArrayList<>();
+		TypedQuery<Prodotto> query = this.em.createNamedQuery("trovaProdotti", Prodotto.class);
+		catalogo = query.getResultList();
+		return catalogo;
+	}
+	
+	public Prodotto cercaProdotto(Long idProdotto) {
+		return this.em.find(Prodotto.class, idProdotto);
+	}
+	
+}
