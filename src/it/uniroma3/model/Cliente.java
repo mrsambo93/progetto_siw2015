a5f255c69fc31a118e7eba8cf41ad2402f2d56d5
regini <nicco93@gmail.com>
2015-06-12 12:30:31 +0200

    Controller Cliente e Facade Cliente

--- src/it/uniroma3/model/Cliente.java
+++ src/it/uniroma3/model/Cliente.java
@@ -33,12 +33,16 @@ public class Cliente {
 	
 	@Column(nullable=false)
 	private Indirizzo indirizzo;
+	
+	@Column(nullable=false)
+	private String password;
 
-	public Cliente(String nome, String cognome,Indirizzo indirizzo, String email){
+	public Cliente(String nome, String cognome,Indirizzo indirizzo, String email, String password){
 		this.nome = nome;
 		this.cognome = cognome;
 		this.indirizzo = indirizzo;
 		this.email = email;
+		this.password = password;
 	}
 
 	public Long getId() {
@@ -91,6 +95,18 @@ public class Cliente {
 
 	public void setDataDiRegistrazione(Date dataDiRegistrazione) {
 		this.dataDiRegistrazione = dataDiRegistrazione;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+	
+	public void setPassword(String password){
+		this.password=password;
+	}
+	
+	public boolean verificaCredenziali(String password){
+		return this.password.equals(password);
 	}
 }
 
---
+++ src/it/uniroma3/model/FacadeCliente.java
@@ -0,0 +1,23 @@
+package it.uniroma3.model;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless(name = "facadeCliente")
+public class FacadeCliente {
+
+	@PersistenceContext(unitName = "unit-progetto")
+	private EntityManager em;
+	
+	public Cliente creaCliente(String nome, String cognome,Indirizzo indirizzo, String email, String password){
+		Cliente cliente = new Cliente(nome, cognome, indirizzo, email, password);
+		this.em.persist(cliente);
+		return cliente;
+	}
+	
+	public boolean autenticaAutente(String email, String password){
+		Cliente cliente = this.em.find(Cliente.class, email);
+		return cliente.verificaCredenziali(password);
+	}
+}
