018b986b27ca8fa32fb0121809beb129d32e535a
Stefano Silvi <sambora93@gmail.com>
2015-06-15 18:27:16 +0200

    per pull

--- src/it/uniroma3/controller/ControllerProdotto.java
+++ src/it/uniroma3/controller/ControllerProdotto.java
@@ -10,7 +10,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @SessionScoped
-@ManagedBean(name = "controllerProdotto")
+@ManagedBean(name = "controllerProdotto", eager=true)
 public class ControllerProdotto {
 	
 	@EJB
